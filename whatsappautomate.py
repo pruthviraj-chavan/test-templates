8f0705ceef0aa2877a779458d8a045a7ec5016be
pruthviraj-chavan <pruthvirajchavan2002@gmail.com>
2023-10-05 11:49:07 +0530

    first

---
+++ whatsappautomate.py
@@ -0,0 +1,8 @@
+import pyautogui as pg
+import time 
+time.sleep(3)
+for i in range(50):
+    pg.write(" for students to learn and grow as developers. Link - https://chat.whatsapp.com/JmKrjnaAhYl6xkOP8Mw9lZ")
+
+    time.sleep(0.3)
+    pg.press('Enter') 
